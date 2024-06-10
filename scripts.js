a608cdd97630d573e684a9d57152ef099b4c8b07
RepairBoy <vijinrobinson14@gmail.com>
2024-06-10 09:20:06 +0530

    added a quiz page and some features

--- scripts.js
+++ scripts.js
@@ -21,10 +21,20 @@ function typeEffect(element, text, delay = 100) {
 
 // Dynamic content loading for character message
 document.addEventListener('DOMContentLoaded', function () {
-    const wordOfTheDay = 'Innovate';
+    
+    if (window.location.pathname === '/quizPage.html') {
+        const myMessage = "1) what is the capital of france?"
+        const characterTextElement = document.getElementById('character-text');
+        typeEffect(characterTextElement, myMessage, 50);
+    }
+    else{
+        const wordOfTheDay = 'Innovate';
     const thoughtOfTheDay = 'Think big, start small, scale fast.';
     const fullMessage = `Welcome to ExQuiZ, the word of the day is ${wordOfTheDay} and the thought of the day is ${thoughtOfTheDay}.`;
 
     const characterTextElement = document.getElementById('character-text');
     typeEffect(characterTextElement, fullMessage, 50);  // Adjust the delay for typing speed
+    }
 });
+
+
