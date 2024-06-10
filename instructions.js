6c0cd5b04da5d485bffb5ffe73ceb1e27d358266
RepairBoy <vijinrobinson14@gmail.com>
2024-06-10 13:34:53 +0530

    added an instructions page

---
+++ instructions.js
@@ -0,0 +1,22 @@
+// Typing effect function
+function typeEffect(element, text, delay = 100) {
+    let index = 0;
+    const chatbox = document.getElementsByClassName('character-message')
+    function type() {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index);
+            index++;
+            chatbox[0].style.transform = `translateY(-${chatbox[0].clientHeight}px)`;
+            setTimeout(type, delay);
+        }
+    }
+    type();
+}
+
+// Content loading for character message
+document.addEventListener('DOMContentLoaded', function () {
+    let fullMessage = "Scroll down for the game instructions.";
+    let characterTextElement = document.getElementById('character-text');
+    typeEffect(characterTextElement, fullMessage, 50);     //change the question using this variable
+});
+
